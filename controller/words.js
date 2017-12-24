5a00b317bc0fbf8a320bb85a742d59809c4f58c3
Ashirogi-Muto <kshitijpandey32@gmail.com>
2017-12-25 00:43:25 +0530

    basic requirement completed

--- controller/words.js
+++ controller/words.js
@@ -1,3 +1,37 @@
+const http = require('http');
+const request = require('request');
 module.exports.findWordCount = (req, res) => {
-	console.log(req.body);
+	var data = '';
+	let words = [];
+	let wordCounts = {}; //make an empty object to store the words and their counts
+	let sortable = []; //array to store sorted counts
+	let userCount = req.body.count;
+	request('http://terriblytinytales.com/test.txt', (err, response, body) => {
+		if (err) {
+			return res.json({
+				'status': false,
+				'message': err
+			});
+		}
+		data = body;
+		// words.push(data.split(/[ ,.\n\t?"]+/));//split the string by space, comma, new line and quotes
+		words = data.split(/[ ,\n\t?"]+/);
+		
+		for (let i = 0; i < words.length; i++) {
+			wordCounts[words[i]] = (wordCounts[words[i]] || 0) + 1; //assign words and their counts 
+		}
+		for (let word in wordCounts) {
+			sortable.push([word, wordCounts[word]]); //make an array of the word and the word count for sorting
+		}
+		sortable.sort((a, b) => {
+			return b[1] - a[1]; //sort the array in DESC order
+		});
+
+		console.log('>>>>>', userCount, sortable.slice(0, userCount));
+		return res.json({
+			'status': true,
+			'message': 'success',
+			data:  sortable.slice(0, userCount)
+		});
+	})
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 require('./routes/routes')(app);
 const port = 3000;
 app.listen(port);
